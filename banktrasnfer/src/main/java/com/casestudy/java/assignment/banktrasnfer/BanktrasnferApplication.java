55554823fc6228f6521fb72fb825cdd92be501c3
sunilkumar <sunilkumar.havapnor@eon.com>
2020-08-03 22:22:23 +0100

    caset study initial build

---
+++ banktrasnfer/src/main/java/com/casestudy/java/assignment/banktrasnfer/BanktrasnferApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.java.assignment.banktrasnfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktrasnferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanktrasnferApplication.class, args);
+	}
+
+}
---
+++ banktrasnfer/src/test/java/com/casestudy/java/assignment/banktrasnfer/BanktrasnferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudy.java.assignment.banktrasnfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanktrasnferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
